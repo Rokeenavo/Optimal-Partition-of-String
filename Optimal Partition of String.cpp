0884e21e6e0f772bc472cb0c9f320519670d68c2
Rokeenavo <66412740+Rokeenavo@users.noreply.github.com>
2023-10-04 13:18:23 +0300

    Добавьте файлы проекта.

---
+++ Optimal Partition of String.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include <string>
+
+using namespace std;
+
+int minSplit(string s) {
+    int n = s.length();
+    int count = 0;  // Количество подстрок
+    unordered_set<char> uniqueChars;  // Уникальные символы в текущей подстроке
+
+    for (int i = 0; i < n; ++i) {
+        if (uniqueChars.count(s[i]) > 0) {
+            // Если символ уже встречался, заканчиваем текущую подстроку
+            count++;
+            uniqueChars.clear();
+        }
+        uniqueChars.insert(s[i]);
+    }
+
+    // Добавляем последнюю подстроку (если она не пуста)
+    if (!uniqueChars.empty()) {
+        count++;
+    }
+
+    return count;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    string s;
+    cout << " Введите строку - ";
+    cin >>  s;
+    int result = minSplit(s);
+    cout << "Минимальное количество подстрок: " << result << endl;
+
+    return 0;
+}
+
+
+
+
